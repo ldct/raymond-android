c0c973eeb7e433f6878e46980d18b3b871e3ec0c
davidlky <david-lky@hotmail.com>
2015-01-17 15:11:28 -0500

    0.2

--- app/src/main/java/com/pennapps/raymond/CategoryActivity.java
+++ app/src/main/java/com/pennapps/raymond/CategoryActivity.java
@@ -1,19 +1,51 @@
 package com.pennapps.raymond;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Transformation;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class CategoryActivity extends ActionBarActivity {
+
+    LinearLayout mContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        mContent = (LinearLayout)findViewById(R.id.splash);
+                AlphaAnimation animation1 = new AlphaAnimation(0.0f, 1.0f);
+                animation1.setDuration(500);
+        animation1.setStartOffset(500);
+            ((TextView)findViewById(R.id.miniheader)).startAnimation(animation1);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                Animation a;
+                a = new ExpandAnimation(100, 60);
+                a.setDuration(1000);
+                mContent.startAnimation(a);
+                //mContent.setLayoutParams(lp);
+                // close this activity
+            }
+        }, 2000);
     }
 
 
@@ -41,5 +73,28 @@ public class CategoryActivity extends ActionBarActivity {
     public void click(View view){
         Intent intent = new Intent(this, CameraActivity.class);
         startActivity(intent);
+    }
+
+    private class ExpandAnimation extends Animation {
+
+        private final float mStartWeight;
+        private final float mDeltaWeight;
+
+        public ExpandAnimation(float startWeight, float endWeight) {
+            mStartWeight = startWeight;
+            mDeltaWeight = endWeight - startWeight;
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) mContent.getLayoutParams();
+            lp.weight = (mStartWeight + (mDeltaWeight * interpolatedTime));
+            mContent.setLayoutParams(lp);
+        }
+
+        @Override
+        public boolean willChangeBounds() {
+            return true;
+        }
     }
 }
