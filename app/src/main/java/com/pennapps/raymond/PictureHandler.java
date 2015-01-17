5e2c99a2632093d307e21e3fabcbf2352733252b
Li Xuanji <xuanji@gmail.com>
2015-01-17 10:29:05 -0500

    UploadImageTask...again

--- app/src/main/java/com/pennapps/raymond/PictureHandler.java
+++ app/src/main/java/com/pennapps/raymond/PictureHandler.java
@@ -1,7 +1,9 @@
 package com.pennapps.raymond;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -9,10 +11,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 public class PictureHandler implements PictureCallback {
 
@@ -62,6 +73,41 @@ public class PictureHandler implements PictureCallback {
         intent.putExtras(bundle);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
+
+    }
+
+    public class UploadImageTask  extends AsyncTask<String, Integer , String> {
+        public byte[] dataToServer;
+
+        @Override
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try {
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    out.close();
+                    responseString = out.toString();
+                } else{
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+                //TODO Handle problems..
+            } catch (IOException e) {
+                //TODO Handle problems..
+            }
+            return responseString;
+        }
+
+        protected void onPostExecute(Long result) {
+            Toast.makeText(context, "done", Toast.LENGTH_LONG).show();
+        }
+
 
     }
 
