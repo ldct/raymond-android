c4d299438cf9f2483c1ca9edb488bc6c34087a3b
Li Xuanji <xuanji@gmail.com>
2015-01-17 12:11:11 -0500

    Add imageuploaderservice

---
+++ app/src/main/java/com/pennapps/raymond/ImageUploaderService.java
@@ -0,0 +1,12 @@
+package com.pennapps.raymond;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ImageUploaderService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
