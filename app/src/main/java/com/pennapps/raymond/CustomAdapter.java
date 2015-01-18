372337a453e406b3cb6579f1b597c9779b8d5f8e
davidlky <david-lky@hotmail.com>
2015-01-18 03:48:48 -0500

    0.3

--- app/src/main/java/com/pennapps/raymond/CustomAdapter.java
+++ app/src/main/java/com/pennapps/raymond/CustomAdapter.java
@@ -2,9 +2,11 @@ package com.pennapps.raymond;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,6 +17,8 @@ public class CustomAdapter extends ArrayAdapter<String> {
     private final Context context;
     private ArrayList arraylist;
     private int layout;
+    private String olddate = "";
+    private String oldlocaiton ="";
 
     public CustomAdapter(Context context, ArrayList arrayList, int layout) {
         super(context, layout, arrayList);
@@ -25,18 +29,94 @@ public class CustomAdapter extends ArrayAdapter<String> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = new View(context);
-        if(layout == R.layout.receipt){
-        rowView = inflater.inflate(layout, parent, false);
-            Receipt current = ((ArrayList<Receipt>)arraylist).get(position);
-            TextView textView = (TextView) rowView.findViewById(R.id.textView);
-            TextView textView1 = (TextView) rowView.findViewById(R.id.textView2);
-            textView.setText(current.getDate());
-            textView1.setText(current.getLocation());
-        }
+        if(convertView ==null) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = new View(context);
+            if (layout == R.layout.receipt) {
+                Receipt current = ((ArrayList<Receipt>) arraylist).get(position);
+                if (current.getDate() != null && current.getDate().equals(olddate) && current.getLocation().equals(oldlocaiton)) {
+                    convertView = inflater.inflate(R.layout.receipt_deet, parent, false);
+                    TextView textView = (TextView) convertView.findViewById(R.id.textView);
+                    TextView textView1 = (TextView) convertView.findViewById(R.id.textView2);
+                    textView.setText(current.getItem());
+                    textView1.setText(current.getPrice());
+                } else {
+                    convertView = inflater.inflate(layout, parent, false);
+                    TextView textView = (TextView) convertView.findViewById(R.id.textView);
+                    TextView textView1 = (TextView) convertView.findViewById(R.id.textView2);
+                    textView.setText(current.getDate());
+                    textView1.setText(current.getLocation());
+                    //part2
+                    TextView textView3 = (TextView) convertView.findViewById(R.id.textView3);
+                    TextView textView4 = (TextView) convertView.findViewById(R.id.textView4);
+                    textView3.setText(current.getItem());
+                    textView4.setText(current.getPrice());
+                }
+                olddate = current.getDate();
+                oldlocaiton = current.getLocation();
+            }
+            else if (layout == R.layout.events) {
+                Event current = ((ArrayList<Event>) arraylist).get(position);
+                convertView = inflater.inflate(layout, parent, false);
+                TextView textView = (TextView) convertView.findViewById(R.id.textView);
+                TextView textView1 = (TextView) convertView.findViewById(R.id.textView2);
+                TextView textView3 = (TextView) convertView.findViewById(R.id.textView3);
+                TextView textView4 = (TextView) convertView.findViewById(R.id.textView4);
+                TextView textView5 = (TextView) convertView.findViewById(R.id.textView5);
 
-        return rowView;
+                textView.setText(current.getName());
+                textView1.setText(current.getDate());
+                textView3.setText(current.getPrice());
+                textView4.setText(current.getTime());
+                textView5.setText(current.getLocation());
+            }
+            else if (layout == R.layout.nutrition) {
+                Nutrition current = ((ArrayList<Nutrition>) arraylist).get(position);
+                convertView = inflater.inflate(layout, parent, false);
+
+                TextView food = (TextView) convertView.findViewById(R.id.food);
+                TextView textView = (TextView) convertView.findViewById(R.id.textView);
+                TextView textView1 = (TextView) convertView.findViewById(R.id.textView1);
+                TextView textView2 = (TextView) convertView.findViewById(R.id.textView2);
+                TextView textView3 = (TextView) convertView.findViewById(R.id.textView3);
+                TextView textView4 = (TextView) convertView.findViewById(R.id.textView4);
+                TextView textView5 = (TextView) convertView.findViewById(R.id.textView5);
+                food.setText(current.getFood());
+                textView.setText(current.getCalories());
+                textView1.setText(current.getFat());
+                textView2.setText(current.getProtein());
+                textView3.setText(current.getCarbs());
+                textView4.setText(current.getSodium());
+                textView5.setText(current.getSugar());
+            }
+            else if (layout == R.layout.bcard) {
+                BusinessCard current = ((ArrayList<BusinessCard>) arraylist).get(position);
+                convertView = inflater.inflate(layout, parent, false);
+
+                TextView name = (TextView) convertView.findViewById(R.id.name);
+                TextView textView = (TextView) convertView.findViewById(R.id.email);
+                TextView textView1 = (TextView) convertView.findViewById(R.id.PhoneNumber);
+                TextView textView2 = (TextView) convertView.findViewById(R.id.company);
+                name.setText(current.getName());
+                textView.setText(current.getEmail());
+                textView1.setText(current.getNumber());
+                textView2.setText(current.getCompany()+" | " +current.getJob());
+            }
+        }
+        return convertView;
+    }
+
+
+
+    @Override
+    public int getCount() {
+        return arraylist.size();
+    }
+
+
+    @Override
+    public long getItemId(int arg0) {
+        return arg0;
     }
 }
--- app/src/main/java/com/pennapps/raymond/Gallery.java
+++ app/src/main/java/com/pennapps/raymond/Gallery.java
@@ -1,26 +1,32 @@
 package com.pennapps.raymond;
 
+import android.app.ListActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 
-public class Gallery extends ActionBarActivity {
+public class Gallery extends ListActivity {
 
     private String type; //category
     @Override
@@ -34,12 +40,27 @@ public class Gallery extends ActionBarActivity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        ListView listView = (ListView)findViewById(R.id.list);
 
         if(type.equals("Receipt")) {
             ArrayList<Receipt> receipts = (new Receipt(null, this)).getCompleteData();
-            listView.setAdapter(new CustomAdapter(this, receipts, R.layout.receipt));
+            setListAdapter(new CustomAdapter(this, receipts, R.layout.receipt));
         }
+        else if(type.equals("Event")) {
+            ArrayList<Event> receipts = (new Event(null, this)).getCompleteData();
+            setListAdapter(new CustomAdapter(this, receipts, R.layout.events));
+        }
+        else if(type.equals("Nutrition")) {
+            ArrayList<Nutrition> receipts = (new Nutrition(null, this)).getCompleteData();
+            setListAdapter(new CustomAdapter(this, receipts, R.layout.nutrition));
+        }
+        else if(type.equals("BusinessCard")) {
+            ArrayList<BusinessCard> receipts = (new BusinessCard(null, this)).getCompleteData();
+            setListAdapter(new CustomAdapter(this, receipts, R.layout.nutrition));
+        }
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        getListView().setMultiChoiceModeListener(new ModeCallback());
+        registerForContextMenu(getListView());
+
 //        LinearLayout ln = (LinearLayout)findViewById(R.id.chart);
 //        ArrayList receipts;
 //        if(type.equals("Receipt")) {
@@ -88,11 +109,17 @@ public class Gallery extends ActionBarActivity {
 //        }
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_gallery, menu);
         return true;
+    }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item =((DataStructure)getListAdapter().getItem(position)).getField(0);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -109,7 +136,6 @@ public class Gallery extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 
     public void refresh(View view){
         //TODO
@@ -133,6 +159,53 @@ public class Gallery extends ActionBarActivity {
 
         }
 
+
+    }
+    private class ModeCallback implements ListView.MultiChoiceModeListener {
+
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu_gallery, menu);
+            mode.setTitle("Select Items");
+            return true;
+        }
+
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return true;
+        }
+
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_settings:
+                    Toast.makeText(getApplicationContext(), "Shared " + getListView().getCheckedItemCount() + " items", Toast.LENGTH_SHORT).show();
+                    mode.finish();
+                    break;
+                default:
+                    Toast.makeText(getApplicationContext(), "Clicked " + item.getTitle(),
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return true;
+        }
+
+        public void onDestroyActionMode(ActionMode mode) {
+        }
+
+        public void onItemCheckedStateChanged(ActionMode mode,
+                                              int position, long id, boolean checked) {
+            final int checkedCount = getListView().getCheckedItemCount();
+            switch (checkedCount) {
+                case 0:
+                    mode.setSubtitle(null);
+                    break;
+                case 1:
+                    mode.setSubtitle("One item selected");
+                    break;
+                default:
+                    mode.setSubtitle("" + checkedCount + " items selected");
+                    break;
+            }
+        }
 
     }
 }
